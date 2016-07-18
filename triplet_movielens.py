52d97d198dfe1aa4b507547da86eedd79322746c
Kyung-Min <kmkim@bi.snu.ac.kr>
2016-07-18 20:02:29 +0900

    Update triplet_movielens.py
    ranking loss implementation using Keras Graph

--- triplet_movielens.py
+++ triplet_movielens.py
@@ -20,101 +20,36 @@ import data
 import metrics
 
 
-def identity_loss(y_true, y_pred):
-
-    return K.mean(y_pred - 0 * y_true)
-
-
-def bpr_triplet_loss(X):
-
-    user_latent, item_latent = X.values()
-    positive_item_latent, negative_item_latent = item_latent.values()
-
-    # BPR loss
-    loss = - K.sigmoid(K.sum(user_latent * positive_item_latent, axis=-1, keepdims=True)
-                       - K.sum(user_latent * negative_item_latent, axis=-1, keepdims=True))
-
-    return loss
-
-
-def margin_triplet_loss(X):
-
-    user_latent, item_latent = X.values()
-    positive_item_latent, negative_item_latent = item_latent.values()
-
-    # Hinge loss: max(0, user * negative_item_latent + 1 - user * positive_item_latent)
-    loss = K.maximum(1.0
-                     + K.sum(user_latent * negative_item_latent, axis=-1, keepdims=True)
-                     - K.sum(user_latent * positive_item_latent, axis=-1, keepdims=True),
-                     0.0)
-
-    return loss
-
-
-def get_item_subgraph(input_shape, latent_dim):
-    # Could take item metadata here, do convolutional layers etc.
-
-    model = Sequential()
-    model.add(Dense(latent_dim, input_shape=input_shape))
-
-    return model
-
-
-def get_user_subgraph(input_shape, latent_dim):
-    # Could do all sorts of fun stuff here that takes
-    # user metadata in.
-
-    model = Sequential()
-    model.add(Dense(latent_dim, input_shape=input_shape))
-
-    return model
+def ranking_loss(y_true, y_pred):
+    pos = y_pred[:,0]
+    neg = y_pred[:,1]
+    loss = -K.sigmoid(pos-neg) # use loss = K.maximum(1.0 + neg - pos, 0.0) if you want to use margin ranking loss
+    return K.mean(loss) + 0 * y_true
 
 
 def get_graph(num_users, num_items, latent_dim):
 
-    batch_input_shape = (1,)
-
     model = Graph()
+    model.add_input(name='user_input', input_shape=(num_users,))
+    model.add_input(name='positive_item_input', input_shape=(num_items,))
+    model.add_input(name='negative_item_input', input_shape=(num_items,))
 
-    # Add inputs
-    model.add_input('user_input', input_shape=(num_users,), batch_input_shape=batch_input_shape)
-    model.add_input('positive_item_input', input_shape=(num_items,), batch_input_shape=batch_input_shape)
-    model.add_input('negative_item_input', input_shape=(num_items,), batch_input_shape=batch_input_shape)
-
-    # Add shared-weight item subgraph
-    model.add_shared_node(get_item_subgraph((num_items,), latent_dim),
-                          name='item_latent',
-                          inputs=['positive_item_input',
-                                  'negative_item_input'],
-                          merge_mode='join')
-    # Add user embedding
-    model.add_node(get_user_subgraph((num_users,), latent_dim),
+    model.add_node(layer=Dense(latent_dim, input_shape = (num_users,)),
                    name='user_latent',
                    input='user_input')
+    model.add_shared_node(layer=Dense(latent_dim, input_shape = (num_items,)), 
+                          name='item_latent', 
+                          inputs=['positive_item_input', 'negative_item_input'],
+                          merge_mode=None, 
+                          outputs=['positive_item_latent', 'negative_item_latent'])
 
-    # Compute loss
-    model.add_node(Lambda(bpr_triplet_loss),
-                   name='triplet_loss',
-                   inputs=['user_latent', 'item_latent'],
-                   merge_mode='join')
+    model.add_node(layer=Activation('linear'), name='user_pos', inputs=['user_latent', 'positive_item_latent'], merge_mode='dot', dot_axes=1)
+    model.add_node(layer=Activation('linear'), name='user_neg', inputs=['user_latent', 'negative_item_latent'], merge_mode='dot', dot_axes=1)
 
-    # Add output
-    model.add_output(name='triplet_loss', input='triplet_loss')
-
-    # Compile using a dummy loss to fit within the Keras paradigm
-    model.compile(loss={'triplet_loss': identity_loss}, optimizer=Adam())#Adagrad(lr=0.1, epsilon=1e-06))
+    model.add_output(name='triplet_loss_out', inputs=['user_pos', 'user_neg'])
+    model.compile(loss={'triplet_loss_out': ranking_loss}, optimizer=Adam())#Adagrad(lr=0.1, epsilon=1e-06))
 
     return model
-
-
-def count_inversions(model, user_features, posititve_item_features, negative_item_features):
-
-    loss = model.predict({'user_input': user_features,
-                          'positive_item_input': posititve_item_features,
-                          'negative_item_input': negative_item_features})['triplet_loss']
-
-    return (loss > 0).mean()
-
 
 if __name__ == '__main__':
 
@@ -150,18 +85,12 @@ if __name__ == '__main__':
 
         model.fit({'user_input': user_features,
                    'positive_item_input': positive_item_features,
-                   'negative_item_input': negative_item_features, 'triplet_loss': np.ones(len(uid))},
-                  validation_data={'user_input': test_user_features,
+                   'negative_item_input': negative_item_features,
+                   'triplet_loss_out': np.ones(len(uid))
+                   },
+                  validation_data=({'user_input': test_user_features,
                                    'positive_item_input': test_positive_item_features,
-                                   'negative_item_input': test_negative_item_features, 'triplet_loss': np.ones(len(uid))},
-                  batch_size=512,
-                  nb_epoch=1, 
-                  verbose=2,
-                  shuffle=True)
+                                   'negative_item_input': test_negative_item_features,
+                                   'triplet_loss_out': np.ones(test_user_features.shape[0])}), batch_size=512, nb_epoch=1, verbose=2,shuffle=True)
 
         print('AUC %s' % metrics.full_auc(model, test))
-        print('Inversions percentage %s' % count_inversions(model,
-                                                            test_user_features,
-                                                            test_positive_item_features,
-                                                            test_negative_item_features))
-
